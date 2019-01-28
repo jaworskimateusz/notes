6e476e709f3bc30f4b6a8973907c663f4d18f25d
jaworskimateusz <jaworskimateuszm@gmail.com>
2019-01-28 22:55:33 +0100

    Testing RegistrationController.class

--- src/test/java/com/jaworskimateusz/controller/RegistrationControllerTest.java
+++ src/test/java/com/jaworskimateusz/controller/RegistrationControllerTest.java
@@ -2,10 +2,15 @@ package com.jaworskimateusz.controller;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -21,8 +26,8 @@ import org.junit.Test;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ApplicationConfig.class, SecurityConfig.class})
 @WebAppConfiguration
-@WebMvcTest(LoginController.class)
-public class LoginControllerTest {
+@WebMvcTest(RegistrationController.class)
+public class RegistrationControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -31,25 +36,20 @@ public class LoginControllerTest {
     private UserService userService;
 	
 	@Test
-	public void testLoginPage() throws Exception {
-		mockMvc.perform(get("/login"))    
+	public void testRegistrationPage() throws Exception {
+		mockMvc.perform(get("/registration"))    
 	      .andExpect(status().isOk())  
-	      .andExpect(view().name("login"));
+	      .andExpect(view().name("registration"))
+	      .andExpect(model().attributeExists("newUser"));
 	}
 	
 	@Test
-	public void testHomePage() throws Exception {
-		mockMvc.perform(get("/home"))    
+	public void testProcessRegistration() throws Exception {
+		mockMvc.perform(post("/process-registration")
+				.with(csrf())
+	            .param("action", "signup")) 
 	      .andExpect(status().isOk())  
 	      .andExpect(view().name("login"));
 	}
-	
-	@Test
-	public void testAccesDeniedPage() throws Exception {
-		mockMvc.perform(get("/access-denied"))    
-	      .andExpect(status().isOk())  
-	      .andExpect(view().name("access-denied"));
-	}
-	
 
 }
